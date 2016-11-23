cd69c2ed7f76a90e889a24a5ed303306286977f8
Justin Riley <justin.t.riley@gmail.com>
2016-11-22 21:04:00 -0500

    initial python scaffolding

---
+++ nvs/cli.py
@@ -0,0 +1,4 @@
+def main():
+    """
+    nvidia-singularity
+    """
---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import os
+
+from setuptools import setup, find_packages
+
+VERSION = 0.1
+version = os.path.join('nvs', '__init__.py')
+execfile(version)
+
+README = open('README.md').read()
+
+setup(
+    name='nvidia-singularity',
+    version=VERSION,
+    packages=find_packages(),
+    author='Justin Riley',
+    author_email='justin_riley@harvard.edu',
+    url="https://github.com/fasrc/nvidia-singularity",
+    description="Utilities to build and run NVIDIA Singularity images",
+    long_description=README,
+    entry_points=dict(console_scripts=[
+        'nvidia-singularity = nvs.cli:main',
+    ]),
+)
