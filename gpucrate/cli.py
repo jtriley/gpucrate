b8119bbb07d9b581bdbb7477ec5f4fd7a0e392cf
Justin Riley <justin.t.riley@gmail.com>
2017-02-16 01:00:48 -0500

    cli: add create command + tests

--- gpucrate/cli.py
+++ gpucrate/cli.py
@@ -1,10 +1,16 @@
+import os
 import argparse
 
+import sh
+
 from gpucrate import utils
+from gpucrate import config
 from gpucrate import logger
+from gpucrate import volume
+from gpucrate import exception
 
 
-def shell(args, **kwargs):
+def shell(**kwargs):
     ns = dict(log=logger.log)
     utils.shell(local_ns=ns)
 
@@ -17,6 +23,12 @@ def get_gpucrate_parser():
                         action='store_true', default=False,
                         help='enable debug output')
     subparsers = parser.add_subparsers()
+    create_parser = subparsers.add_parser('create')
+    create_parser.add_argument(
+        '--volume-root', required=False,
+        help='path where GPU driver volumes are created'
+    )
+    create_parser.set_defaults(func=create)
     shell_parser = subparsers.add_parser('shell')
     shell_parser.set_defaults(func=shell)
     return parser
@@ -25,5 +37,29 @@ def get_gpucrate_parser():
 def main(**kwargs):
     parser = get_gpucrate_parser()
     args = parser.parse_args(args=kwargs.pop('args', None))
-    logger.configure_gpucrate_logging(debug=args.debug)
-    args.func(args, **kwargs)
+    args_dict = vars(args)
+    logger.configure_gpucrate_logging(debug=debug)
+    try:
+        args.func(**args_dict)
+    except sh.CommandNotFound as e:
+        logger.log.error("required command not found: {e}".format(e=e))
+        exit(2)
+    except exception.GpuCrateException as e:
+        logger.log.error(e.message)
+        exit(1)
+
+
+def create(volume_root=None, **kwargs):
+    volume_root = (volume_root or os.environ.get('GPUCRATE_VOLUME_ROOT') or
+                   config.get('volume_root'))
+    if not volume_root:
+        raise exception.NoVolumeRoot
+    if os.geteuid() != 0:
+        raise exception.RootRequired
+    if not os.path.isdir(volume_root):
+        logger.log.info("Volume root {volume_root} does not exist".format(
+            volume_root=volume_root))
+        os.makedirs(volume_root)
+    vols = volume.lookup_volumes()
+    for vol in vols:
+        volume.create(volume_root, vols)
--- gpucrate/tests/test_cli.py
+++ gpucrate/tests/test_cli.py
@@ -1,10 +1,17 @@
-import pytest
+import os
 
+import sh
 import mock
+import pytest
+import testfixtures
 
 from gpucrate import cli
+from gpucrate import config
+from gpucrate import exception
 
 
+ROOT = mock.MagicMock(return_value=0)
+NON_ROOT = mock.MagicMock(return_value=1)
 SHELL = mock.MagicMock()
 
 
@@ -19,3 +26,86 @@ def test_shell():
     SHELL.reset_mock()
     cli.main(args=['shell'], test=True)
     SHELL.assert_called_once_with(local_ns=dict(log=cli.logger.log))
+
+
+@mock.patch.dict('os.environ', PATH='')
+def _load_non_existent_sh_cmd(*args, **kwargs):
+    sh.Command("doesnotexist")
+
+
+@mock.patch.object(cli, 'create', _load_non_existent_sh_cmd)
+def test_shell_cmd_not_found():
+    with pytest.raises(SystemExit) as excinfo:
+        with testfixtures.LogCapture() as log_capture:
+            cli.main(args=['create'])
+    log_capture.check(
+        ('gpucrate',
+         'ERROR',
+         'required command not found: doesnotexist'),
+    )
+    assert excinfo.value.code == 2
+
+
+def _raise_gpucrate_error(*args, **kwargs):
+    raise exception.GpuCrateException('gpucrate error')
+
+
+@mock.patch.object(cli, 'create', _raise_gpucrate_error)
+def test_gpu_crate_error():
+    with pytest.raises(SystemExit) as excinfo:
+        with testfixtures.LogCapture() as log_capture:
+            cli.main(args=["create"])
+    log_capture.check(
+        ('gpucrate',
+         'ERROR',
+         'gpucrate error')
+    )
+    assert excinfo.value.code == 1
+
+
+@mock.patch.object(config, '_CONFIG', config._CONFIG.copy())
+def test_create_no_volume_root():
+    with pytest.raises(SystemExit):
+        with testfixtures.LogCapture() as log_capture:
+            with testfixtures.TempDirectory() as d:
+                d.write('config', 'volume_root:')
+                config.load(path=os.path.join(d.path, 'config'))
+                cli.main(args=['create'])
+    log_capture.check(
+        ('gpucrate',
+         'ERROR',
+         exception.NoVolumeRoot.message),
+    )
+
+
+@mock.patch.object(os, 'geteuid', NON_ROOT)
+def test_create_root_required():
+    with pytest.raises(SystemExit):
+        with testfixtures.LogCapture() as log_capture:
+            cli.main(args=['create'])
+    log_capture.check(
+        ('gpucrate',
+         'ERROR',
+         exception.RootRequired.message),
+    )
+
+
+@mock.patch.object(os, 'geteuid', ROOT)
+@mock.patch.object(config, '_CONFIG', config._CONFIG.copy())
+@mock.patch.object(cli.volume, 'lookup_volumes',
+                   mock.MagicMock(return_value=[None]))
+@mock.patch.object(cli.volume, 'create',
+                   mock.MagicMock())
+def test_create_volume_root_dne():
+    with testfixtures.LogCapture() as log_capture:
+        with testfixtures.TempDirectory() as d:
+            vroot = os.path.join(d.path, 'vroot')
+            d.write('config', 'volume_root: {vroot}'.format(vroot=vroot))
+            config.load(path=os.path.join(d.path, 'config'))
+            cli.main(args=['create'])
+            assert os.path.isdir(vroot)
+    log_capture.check(
+        ('gpucrate',
+         'INFO',
+         'Volume root {vroot} does not exist'.format(vroot=vroot))
+    )
