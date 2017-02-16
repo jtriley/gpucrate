ba5c0933cd51ee1e465fd18790609451c9cd2603
Justin Riley <justin.t.riley@gmail.com>
2017-02-15 20:56:50 -0500

    utils: replace distutils.spawn with sh.Command

--- gpucrate/utils.py
+++ gpucrate/utils.py
@@ -1,7 +1,6 @@
 import os
 
-import distutils.spawn
-
+import sh
 import pynvml
 
 from gpucrate.logger import log
@@ -20,7 +19,7 @@ def which(prog):
     """
     Returns realpath of program
     """
-    return os.path.realpath(distutils.spawn.find_executable(prog))
+    return os.path.realpath(sh.Command(prog)._path)
 
 
 def shell(local_ns={}):
